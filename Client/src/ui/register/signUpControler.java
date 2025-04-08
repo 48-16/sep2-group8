b36bc0d65af7791acc329e2a6af56aa3924d0d0f
48-16 <355460@viauc.dk>
2025-04-08 16:24:23 +0200

    made a basic ui

---
+++ Client/src/ui/register/signUpControler.java
@@ -0,0 +1,42 @@
+package ui.register;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class signUpControler {
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button cancelButton;
+
+
+    @FXML
+    private void register() {
+
+    }
+    @FXML
+    private void cancel() {
+
+    }
+
+
+
+
+
+
+
+}
