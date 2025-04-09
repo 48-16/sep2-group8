2b9a38fb705588161db72196b158b01b1b265245
Iamagicat <128327457+Iamagicat@users.noreply.github.com>
2025-04-09 16:51:52 +0200

    Appointment.java

--- Sep2/Appointment/src/Appointment.java
+++ Sep2/Appointment/src/Appointment.java
@@ -6,23 +6,20 @@ public class Appointment
   private Client client; //ig we gonna call it like this
   private Admin admin; // baber who gonna take an appointment
   private AppointmentDate appointmentDate;
-  private double price;// should we do price for appointment or on place?
 
-  public Appointment(String id, Client client, Admin admin, AppointmentDate appointmentDate, double price) {
+  public Appointment(String id, Client client, Admin admin, AppointmentDate appointmentDate) {
     this.id = id;
     this.client = client;
     this.admin = admin;
     this.appointmentDate = appointmentDate;
-    this.price = price;
   }
 
   public String toString() {
     return "Appointment id" + id + "\n"
-            + "Client: " + client + "\n" // for barber and client maybe I should use add name method
-            + "Barber: " + admin//idk??
-            + "\n"
-            + "Date: " + appointmentDate + "\n"
-            +"Price: " + price + "\n";
+        + "Client: " + client + "\n" // for barber and client maybe I should use add name method
+        + "Barber: " + admin//idk??
+        + "\n"
+        + "Date: " + appointmentDate + "\n";
   }
 
 
